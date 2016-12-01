7e58c60c434041c420a8f85bc8e8892b5bdf2203
Roman Ganchin <rganchin@umass.edu>
2016-12-01 14:20:09 -0500

    failbook

--- client/app/server.js
+++ client/app/server.js
@@ -1,18 +1,80 @@
 import {readDocument, writeDocument, addDocument, deleteDocument, getCollection} from './database.js';
 
 /**
- * Emulates how a REST call is *asynchronous* -- it calls your function back
- * some time in the future with data.
- */
+* Emulates how a REST call is *asynchronous* -- it calls your function back
+* some time in the future with data.
+*/
 function emulateServerReturn(data, cb) {
   setTimeout(() => {
     cb(data);
   }, 4);
 }
-
+var token = 'eyJpZCI6NH0=';
 /**
- * Resolves a feed item. Internal to the server, since it's synchronous.
- */
++* Properly configure+send an XMLHttpRequest with error handling,
++* authorization token, and other needed properties.
++*/
+function sendXHR(verb, resource, body, cb) {
+  var xhr = new XMLHttpRequest();
+  xhr.open(verb, resource);
+  xhr.setRequestHeader('Authorization', 'Bearer ' + token);
+  // The below comment tells ESLint that FacebookError is a global.
+  // Otherwise, ESLint would complain about it! (See what happens in Atom if
+  // you remove the comment...)
+  /* global FacebookError */
+  // Response received from server. It could be a failure, though!
+  xhr.addEventListener('load', function() {
+    var statusCode = xhr.status;
+    var statusText = xhr.statusText;
+    if (statusCode >= 200 && statusCode < 300) {
+      // Success: Status code is in the [200, 300) range.
+      // Call the callback with the final XHR object.
+      cb(xhr);
+    } else {
+      // Client or server error.
+      // The server may have included some response text with details concerning
+      // the error.
+      var responseText = xhr.responseText;
+      FacebookError('Could not ' + verb + " " + resource + ": Received " +
+      statusCode + " " + statusText + ": " + responseText);
+    }
+  });
+  // Time out the request if it takes longer than 10,000
+  // milliseconds (10 seconds)
+  xhr.timeout = 10000;
+  // Network failure: Could not connect to server.
+  xhr.addEventListener('error', function() {
+    FacebookError('Could not ' + verb + " " + resource +
+    ": Could not connect to the server.");
+  });
+  // Network failure: request took too long to complete.
+  xhr.addEventListener('timeout', function() {
+    FacebookError('Could not ' + verb + " " + resource +
+    ": Request timed out.");
+  });
+  switch (typeof(body)) {
+    case 'undefined':
+    // No body to send.
+    xhr.send();
+    break;
+    case 'string':
+    // Tell the server we are sending text.
+    xhr.setRequestHeader("Content-Type", "text/plain;charset=UTF-8");
+    xhr.send(body);
+    break;
+    case 'object':
+    // Tell the server we are sending JSON.
+    xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+    // Convert body into a JSON string.
+    xhr.send(JSON.stringify(body));
+    break;
+    default:
+    throw new Error('Unknown body type: ' + typeof(body));
+  }
+}
+/**
+* Resolves a feed item. Internal to the server, since it's synchronous.
+*/
 function getFeedItemSync(feedItemId) {
   var feedItem = readDocument('feedItems', feedItemId);
   // Resolve 'like' counter.
@@ -28,22 +90,18 @@ function getFeedItemSync(feedItemId) {
 }
 
 /**
- * Emulates a REST call to get the feed data for a particular user.
- */
+* Emulates a REST call to get the feed data for a particular user.
+*/
 export function getFeedData(user, cb) {
-  var xhr = new XMLHttpRequest();
-  xhr.open('GET', '/user/4/feed');
-  xhr.setRequestHeader('Authorization', 'Bearer eyJpZCI6NH0=');
-  xhr.addEventListener('load', function() {
+    sendXHR('GET', '/user/4/feed', undefined, (xhr) => {
     // Call the callback with the data.
     cb(JSON.parse(xhr.responseText));
   });
-  xhr.send();
 }
 
 /**
- * Adds a new status update to the database.
- */
+* Adds a new status update to the database.
+*/
 export function postStatusUpdate(user, location, contents, cb) {
   // If we were implementing this for real on an actual server, we would check
   // that the user ID is correct & matches the authenticated user. But since
@@ -83,8 +141,8 @@ export function postStatusUpdate(user, location, contents, cb) {
 }
 
 /**
- * Adds a new comment to the database on the given feed item.
- */
+* Adds a new comment to the database on the given feed item.
+*/
 export function postComment(feedItemId, author, contents, cb) {
   var feedItem = readDocument('feedItems', feedItemId);
   feedItem.comments.push({
@@ -99,9 +157,9 @@ export function postComment(feedItemId, author, contents, cb) {
 }
 
 /**
- * Updates a feed item's likeCounter by adding the user to the likeCounter.
- * Provides an updated likeCounter in the response.
- */
+* Updates a feed item's likeCounter by adding the user to the likeCounter.
+* Provides an updated likeCounter in the response.
+*/
 export function likeFeedItem(feedItemId, userId, cb) {
   var feedItem = readDocument('feedItems', feedItemId);
   // Normally, we would check if the user already liked this comment.
@@ -114,9 +172,9 @@ export function likeFeedItem(feedItemId, userId, cb) {
 }
 
 /**
- * Updates a feed item's likeCounter by removing the user from the likeCounter.
- * Provides an updated likeCounter in the response.
- */
+* Updates a feed item's likeCounter by removing the user from the likeCounter.
+* Provides an updated likeCounter in the response.
+*/
 export function unlikeFeedItem(feedItemId, userId, cb) {
   var feedItem = readDocument('feedItems', feedItemId);
   // Find the array index that contains the user's ID.
@@ -134,8 +192,8 @@ export function unlikeFeedItem(feedItemId, userId, cb) {
 }
 
 /**
- * Adds a 'like' to a comment.
- */
+* Adds a 'like' to a comment.
+*/
 export function likeComment(feedItemId, commentIdx, userId, cb) {
   var feedItem = readDocument('feedItems', feedItemId);
   var comment = feedItem.comments[commentIdx];
@@ -146,8 +204,8 @@ export function likeComment(feedItemId, commentIdx, userId, cb) {
 }
 
 /**
- * Removes a 'like' from a comment.
- */
+* Removes a 'like' from a comment.
+*/
 export function unlikeComment(feedItemId, commentIdx, userId, cb) {
   var feedItem = readDocument('feedItems', feedItemId);
   var comment = feedItem.comments[commentIdx];
@@ -161,8 +219,8 @@ export function unlikeComment(feedItemId, commentIdx, userId, cb) {
 }
 
 /**
- * Updates the text in a feed item (assumes a status update)
- */
+* Updates the text in a feed item (assumes a status update)
+*/
 export function updateFeedItemText(feedItemId, newContent, cb) {
   var feedItem = readDocument('feedItems', feedItemId);
   // Update text content of update.
@@ -172,8 +230,8 @@ export function updateFeedItemText(feedItemId, newContent, cb) {
 }
 
 /**
- * Deletes a feed item.
- */
+* Deletes a feed item.
+*/
 export function deleteFeedItem(feedItemId, cb) {
   // Assumption: The current user authored this feed item.
   deleteDocument('feedItems', feedItemId);
@@ -198,8 +256,8 @@ export function deleteFeedItem(feedItemId, cb) {
 }
 
 /**
- * Searches for feed items with the given text.
- */
+* Searches for feed items with the given text.
+*/
 export function searchForFeedItems(userId, queryText, cb) {
   // trim() removes whitespace before and after the query.
   // toLowerCase() makes the query lowercase.
